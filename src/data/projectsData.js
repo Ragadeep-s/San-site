1ce42d98634dd211ea85afddcbb7d9f25cde8573
Ragadeep-s <sivagnanam2771@gmail.com>
2025-05-08 15:07:50 +0530

    Create projectsData.js

---
+++ src/data/projectsData.js
@@ -0,0 +1,78 @@
+const projectsData = [
+  {
+    name: 'Short Films',
+    delay: 200,
+    items: [
+      {
+        title: 'Aalaporan thamizhan',
+        image: 'aalaporan-thamizhan.jpg', // Make sure this filename matches your image file
+        briefDescription: 'A group of friends reunites to stop a big scam.',
+      },
+      {
+        title: 'Kanave Kalaiyaathe',
+        image: 'kanave-kalaiyaathe.jpg',
+        briefDescription: 'A rich man dreams of being a middle-class loser.',
+      },
+      {
+        title: 'Muthal Maatram Un Kaiyil',
+        image: 'muthal-maatram.jpg',
+        briefDescription: 'A judge at an environmental film fest has a realization.',
+      },
+      {
+        title: 'Unnakkaga Vaazha Ninaikiren',
+        image: 'unnakkaga-vaazha.jpg',
+        briefDescription: 'A wife helps her husband overcome his past love.',
+      },
+      {
+        title: 'Scared',
+        image: 'venpani-malare.jpg', // Using a placeholder as no specific image was given
+        briefDescription: 'A chain smoker dies in a dream and quits.',
+      },
+    ],
+  },
+  {
+    name: 'Pilot Films',
+    delay: 400,
+    items: [
+      {
+        title: 'Mudintha Kathai Thodarvathillai (Iraivan Aetinile)',
+        image: 'mudintha-kathai.jpg',
+        briefDescription: 'A reporter investigates a murder with caste implications.',
+      },
+      {
+        title: 'The Last Wish',
+        image: 'the-last-wish.jpg',
+        briefDescription: 'A drug addict has a hallucinatory experience.',
+      },
+    ],
+  },
+  {
+    name: 'Webseries',
+    delay: 600,
+    items: [
+      {
+        title: 'Venpani Malare',
+        image: 'venpani-malare.jpg',
+        briefDescription: 'A Rom-Com series about cultural differences and love.',
+      },
+    ],
+  },
+  {
+    name: 'Album Songs',
+    delay: 800,
+    items: [
+      {
+        title: 'Aval Peyar Dhatchaeni',
+        image: 'aval-peyar-dhatchaeni.jpg',
+        briefDescription: 'An album song inspired by a Tamil movie.',
+      },
+      {
+        title: 'Avalo Athirai',
+        image: 'avalo-athirai.jpg',
+        briefDescription: 'A love feel music video.',
+      },
+    ],
+  },
+];
+
+export default projectsData;
